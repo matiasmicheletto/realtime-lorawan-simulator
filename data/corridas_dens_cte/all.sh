3b89555a41c58b64bbbca087fdf8cc4f358b9d78
matias <matiasmicheletto@gmail.com>
2022-11-16 19:43:20 -0300

    Constant density scripts

--- data/corridas_dens_cte/all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./dens_cte.sh 1
-./dens_cte.sh 0
-./dens_cte.sh 3
-./dens_cte.sh 2
---
+++ data/corridas_dens_cte/soft.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./dens_cte_soft.sh 1
+./dens_cte_soft.sh 0
+./dens_cte_soft.sh 3
+./dens_cte_soft.sh 2
